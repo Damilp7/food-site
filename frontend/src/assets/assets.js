0d76ffd14e2c40437550a37c4ea8c7cbebc114a4
Damilp7 <ayoolabamgbelu@gmail.com>
2024-06-13 12:33:51 +0100

    minor changes to the css

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -65,9 +65,9 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 import takeout from './takeout.png'
-import basket_icon2 from './basket_icon2.png'
+import basket_icon2 from './shopping_basket.png'
 import bag_icon2 from './bag_icon2.png'
-import search_icon2 from './search_icon2.png'
+import search_icon2 from './explorer.png'
 import logout_icon2 from './logout_icon2.png'
 
 export const assets = {
