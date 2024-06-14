19e565accd84b1ef2d748df77d0745d9b2ef1ba6
Damilp7 <ayoolabamgbelu@gmail.com>
2024-06-14 11:57:21 +0100

    update to frontend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,7 +10,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing customer's order from the frontend
 const placeOrder = async (req,res) => {
 
-  const frontend_url = "http://localhost:5174";
+
+  // const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://food-site-client.vercel.app/";
 
   try {
     const newOrder = new orderModel({
